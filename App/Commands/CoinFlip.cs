325591aa4fad084f21cc7d5eca52b53b22a03c8f
WilliamDanielQuinones <45363476+WilliamDanielQuinones@users.noreply.github.com>
2022-04-23 13:28:12 -0400

    coinflip cmd

---
+++ App/Commands/CoinFlip.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Commands;
+
+public  class CoinFlip : ModuleBase<SocketCommandContext>
+{
+    readonly Random rand = new();
+
+    [Command("flip")]
+    [Name("flip")]
+    [Summary("Flips a coin")]
+    [Alias("coin", "coinflip")]
+    public Task Flip() => Context.Channel.SendMessageAsync(rand.Next(1, 3) == 1 ? "heads" : "tails");
+}
